db20ceb41e0d46c087633a124226a5d907f19b70
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-20 14:01:05 -0500

    Update Info

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,5 +1,10 @@
 SHOW databases;
 USE employees;
 SELECT database();
-DESCRIBE albums;
-SHOW CREATE DATABASE albums_db;
+DESCRIBE employees;
+# int, date, varchar, enum = Data types
+# Numeric Tables: I think all tales have a numeric type column
+# String Type: I think that all tables would have a string type column
+# Date Types: I think that employees, employees_with_departments, and departments will have date type column
+#The employee table will list the department that the employee is associated with, the department table show department specific demographics.
+SHOW CREATE TABLE dept_manager;
