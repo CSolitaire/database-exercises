dd59aa6385f09a0913a67305e48cb3db751f89ce
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-20 14:33:24 -0500

    More Updates

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -9,8 +9,8 @@ describe employees_with_departments;
 describe salaries;
 describe titles;
 # int, date, varchar, enum = Data types
-# Numeric Column: dept_emp, dept_manager, employees, employees_with_departments, salaries, titles
-# String Calumn: departments, employees, employees_with_departments, titles
-# Date Types: dept_emp, dept_manager, employees, salaries, titles
+# Numeric Column: salaries
+# String Calumn: titles, employees
+# Date Types: employees, titles, salaries, dept_manager
 #The employee table lists employee number and name, the department table lists department number and department name. They are linked together in the employees_with_departments table
 #SHOW CREATE TABLE dept_manager;
