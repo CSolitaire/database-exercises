c16ead38ad3668730eccc42a7e9b19d6b9d9b782
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-20 14:21:16 -0500

    Revised Answers

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -2,9 +2,15 @@ SHOW databases;
 USE employees;
 SELECT database();
 DESCRIBE employees;
+DESCRIBE departments;
+Describe dept_emp;
+describe dept_manager;
+describe employees_with_departments;
+describe salaries;
+describe titles;
 # int, date, varchar, enum = Data types
 # Numeric Column: dept_emp, dept_manager, employees, employees_with_departments, salaries, titles
 # String Calumn: departments, employees, employees_with_departments, titles
 # Date Types: dept_emp, dept_manager, employees, salaries, titles
 #The employee table lists employee number and name, the department table lists department number and department name. They are linked together in the employees_with_departments table
-SHOW CREATE TABLE dept_manager;
+#SHOW CREATE TABLE dept_manager;
