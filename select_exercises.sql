1b5379cb36df9c9c340d5ad97f46a9475ecb0ddc
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-20 18:03:11 -0500

    Completed Exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,6 +3,7 @@ USE albums_db;
 select database();
 show tables;
 describe albums;
+
 # Question 4a
 select name
 from albums
@@ -27,8 +28,25 @@ select * from albums;
 #Question 4d
 select name
 from albums
-where release_date Between 1990 and 1999;
+where release_date Between 1990 and 1999
+order by name;
 
 select * from albums;
 
 #Question 4e
+select name
+from albums
+where sales < 20.0
+Order by name;
+
+select * from albums;
+
+#Question 4f
+select genre, name 'Album_Name'
+from albums
+where genre = 'Rock';
+#Does not include 'hard rock' or 'progressive rock' because the search is specific to string literal 'rock'. To include other 'rock' like genre must use like operator
+
+select genre 'Music_Genre', name 'Album_Name'
+from albums
+where genre like '%Rock%';
