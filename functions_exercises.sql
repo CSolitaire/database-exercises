ce3e2e999ee0899b82fd9044a6b1107ba2fe849d
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-22 10:40:46 -0500

    Exercise Complete

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -3,7 +3,7 @@ select database();
 select * from employees
 limit 10;
 
--- Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
+-- 2.Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
 
 select concat( first_name,' ',`last_name`) as full_name
 from employees
@@ -21,7 +21,7 @@ where last_name like 'e%'
 or last_name like '%e'
 order by emp_no;
 
--- Convert the names produced in your last query to all uppercase.
+-- 3.Convert the names produced in your last query to all uppercase.
 
 select upper(concat(first_name,' ',`last_name`)) as full_name
 from employees
@@ -39,22 +39,18 @@ where last_name like 'e%'
 or last_name like '%e'
 order by emp_no;
 
--- For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE())
+-- 4.For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE())
 
-select concat( first_name,' ',`last_name`) as full_name
-            ,hire_date()
+select concat( first_name,' ',`last_name`) as full_name 		
+       , datediff(curdate(), hire_date) as days_worked
+       , hire_date
 from employees
-where hire_date > '1990' and hire_date <'2000'
+where hire_date > '1990' and hire_date < '2000'
 and birth_date like "%12-25"
-              Case
-              When datediff(day, curdate(), hire_date) > 0
-              END
 order by hire_date DESC;
 
-select datediff(d, curdate(), hire_date)
-from employees;
 
--- Find the smallest and largest salary from the salaries table.
+-- 5.Find the smallest and largest salary from the salaries table.
 
 select *
 from salaries;
@@ -62,7 +58,7 @@ from salaries;
 select min(salary) as min_salary, max(salary) as max_salary
 from salaries;
 
--- Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
+-- 6.Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
 
 select * 
 from employees
@@ -76,7 +72,5 @@ select concat(lower(substring(first_name,1,1))
 from employees
 limit 10;
 
-select substring(birth_date,3,2)
-from employees;
 
 
