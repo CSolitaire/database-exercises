e4eeab83b977aa2774440c79695f18600c47544b
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-22 13:13:14 -0500

    Halway there

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -14,15 +14,23 @@ group by last_name;
 
 -- 4. Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
 
-select first_name, distinct last_name
+select distinct first_name, last_name
 from employees
 where last_name like 'e%e'
-group by last_name;
+group by first_name, last_name;
 
 -- 5. Find the unique last names with a 'q' but not 'qu'. Your results should be:
 
-select distinct first_name, last_name
+select distinct last_name
 from employees
 where last_name like '%q%'
 and last_name not like '%qu%'
 group by last_name;
+
+-- 6. Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+
+select distinct first_name, last_name, count(*) as number_shared
+from employees
+where last_name like 'e%e'
+group by first_name, last_name
+order by number_shared desc;
