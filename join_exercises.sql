be70a5cf4b4df3e88389ee29dfacaa4473d0c9a1
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-23 08:10:15 -0500

    inital commit

---
+++ join_exercises.sql
@@ -0,0 +1,9 @@
+use employees;
+
+-- Join Example Database
+
+-- 1.Use the join_example_db. Select all the records from both the users and roles tables.
+
+-- 2.Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
+
+-- 3.Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use count and the appropriate join type to get a list of roles along with the number of users that has the role. Hint: You will also need to use group by in the query.
