668c89e26b4d803bc5e4a3575b0356799d4360e5
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-23 10:14:56 -0500

    Exercise Complete

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -29,11 +29,11 @@ group by last_name;
 
 -- 6. Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
 
-select distinct first_name, last_name, count(*) as number_shared
+select distinct first_name, count(last_name)
 from employees
 where last_name like 'e%e'
 group by first_name, last_name
-order by number_shared desc;
+order by last_name;
 
 -- 7. Update your query for 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names
 
