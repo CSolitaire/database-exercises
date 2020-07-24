00c7403902590e7dacd658cb76ff9bd1fd8dd3ee
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-24 15:04:52 -0500

    Question 1

---
+++ subqueries_exercises.sql
@@ -0,0 +1,16 @@
+
+-- 1.Find all the employees with the same hire date as employee 101010 using a sub-query. 
+
+select first_name, last_name, hire_date
+from employees
+where hire_date in (
+	select hire_date
+	from employees
+	where emp_no = 101010
+);
+
+-- 2.Find all the titles held by all employees with the first name Aamod.
+
+-- 3.How many people in the employees table are no longer working for the company?
+
+-- 4.Find all the current department managers that are female.
