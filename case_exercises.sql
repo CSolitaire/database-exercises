4a321c7598b9f3c0e58862fb3a4fdc5218392d1a
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-28 11:33:10 -0500

    Completed Number 2

--- case_exercises.sql
+++ case_exercises.sql
@@ -13,3 +13,15 @@ from employees as e
 join dept_emp as dp using(emp_no)
 ;
 
+-- 2. Write a query that returns all employee names, and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name. 
+
+select
+ 	concat(e.first_name, ' ', e.last_name),
+ 	case
+ when substring(lower(e.last_name),1,1) in ('a','b','c','d','e','f','g','h') then 'A-H'
+ when substring(lower(e.last_name),1,1) in ('i','j','k','l','m','n','o','p','q') then 'I-Q'
+ when substring(lower(e.last_name),1,1) in ('r','s','t','u','v','w','x','y','z') then 'R-Z'
+ 		end as alpha_group
+from employees as e
+order by e.last_name asc
+;
