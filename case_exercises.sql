980820667db51cda9a482d435428b27f09f82a98
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-28 12:00:40 -0500

    Exercise Complete

--- case_exercises.sql
+++ case_exercises.sql
@@ -35,8 +35,8 @@ SELECT
 		when substring(e.birth_date, 1, 4) in ('1960','1961', '1962','1963','1964', '1965','1966','1967','1968','1969') then '60'
 	    when substring(e.birth_date, 1, 4) in ('1970','1971', '1972','1973','1974', '1975','1976','1977','1978','1979') then '70'
 	    when substring(e.birth_date, 1, 4) in ('1980','1981', '1982','1983','1984', '1985','1986','1987','1988','1989') then '80'
-		end as emp_born_each_decade
+		end as decade_born
 from employees as e
 join dept_emp as dp using(emp_no)
 where dp.to_date > now()
-order by emp_born_each_decade
+order by decade_born
